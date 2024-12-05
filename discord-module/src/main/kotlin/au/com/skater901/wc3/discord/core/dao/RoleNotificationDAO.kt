fe767d3238b986022c4c5dda04abb5f28db269b9
Skater901 <skaterx901@hotmail.com>
2024-12-05 18:05:45 +1100

    Bump library versions.

--- discord-module/src/main/kotlin/au/com/skater901/wc3/discord/core/dao/RoleNotificationDAO.kt
+++ discord-module/src/main/kotlin/au/com/skater901/wc3/discord/core/dao/RoleNotificationDAO.kt
@@ -1,4 +1,9 @@
 package au.com.skater901.wc3.discord.core.dao
 
-interface Hello {
+import net.dv8tion.jda.api.entities.Role
+
+internal interface RoleNotificationDAO {
+    suspend fun save(channelId: Long, role: Role)
+
+    suspend fun find(channelId: Long): Role?
 }
